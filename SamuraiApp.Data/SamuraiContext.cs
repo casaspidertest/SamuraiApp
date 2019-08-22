4b46388efd81a2c85dcf605c90057def701cf4f1
CasaSpider <peter.nijhuis@brabantwater.nl>
2019-08-22 09:49:20 +0000

    first commit

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:peitycdvj2.database.windows.net,1433;Initial Catalog=SamuraiAppData;Persist Security Info=False;User ID=cbamanager;Password=Feyenoord1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
